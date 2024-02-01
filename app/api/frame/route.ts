01368ca3e359bf9678f207a1c6ea7ec1e2951085
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-01 11:20:32 -0800

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -3,36 +3,39 @@ import { NextRequest, NextResponse } from 'next/server';
 import {kv} from "@vercel/kv";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  // let accountAddress: string | undefined = '';
+  let accountAddress: string | undefined = '';
+  let buttonIndex: number = 0;
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body);
 
-  // if (isValid) {
-  //   try {
-  //     accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
-  // }
-
-  const buttonIndex = message?.buttonIndex || 0;
-  const fid = message?.fid || 0;
-
-  // Check if voted before
-  const voteExists = await kv.sismember(`voted`, fid)
-  if (!voteExists) {
-    let multi = kv.multi();
-    if (buttonIndex === 1) {
-      // Increment value for 49ers in kv database
-      multi.incr('49ers');
+  if (isValid) {
+    try {
+      accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
+    } catch (err) {
+      console.error(err);
     }
-    else if (buttonIndex === 2) {
-      // Increment value for Chiefs in kv database
-      multi.incr('chiefs');
+  
+
+    buttonIndex = message?.buttonIndex || 0;
+    const fid = message?.fid || 0;
+
+    // Check if voted before
+    const voteExists = await kv.sismember(`voted`, fid)
+    if (!voteExists) {
+      let multi = kv.multi();
+      if (buttonIndex === 1) {
+        // Increment value for 49ers in kv database
+        multi.incr('49ers');
+      }
+      else if (buttonIndex === 2) {
+        // Increment value for Chiefs in kv database
+        multi.incr('chiefs');
+      }
+      multi.sadd(`voted`, fid);
+      await multi.exec();
     }
-    multi.sadd(`voted`, fid);
-    await multi.exec();
-  }
+
+  } 
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
