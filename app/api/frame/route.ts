534ad49000e911f65a5ce65805ecb0129c897298
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-01-31 16:23:07 -0800

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -16,7 +16,6 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="https://zizzamia.xyz/park-2.png" />
-    <meta property="fc:frame:post_url" content="https://zizzamia.xyz/api/frame" />
   </head></html>`);
 }
 
