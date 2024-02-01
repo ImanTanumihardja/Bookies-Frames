28025cc582e23ce5cb08be792a7b06b24455c0b3
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-01-31 17:53:09 -0800

    Added chief gif

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -2,20 +2,23 @@ import { FrameRequest, getFrameAccountAddress, getFrameMessage } from '@coinbase
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  let accountAddress: string | undefined = '';
+  // let accountAddress: string | undefined = '';
   const body: FrameRequest = await req.json();
   const { isValid, message } = await getFrameMessage(body);
-  if (isValid) {
-    try {
-      accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
-    } catch (err) {
-      console.error(err);
-    }
-  }
+
+  // if (isValid) {
+  //   try {
+  //     accountAddress = await getFrameAccountAddress(message, { NEYNAR_API_KEY: 'NEYNAR_API_DOCS' });
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
+
+  const buttonIndex = body.buttonIndex as number;
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://bookies-frames.vercel.app/49ers.gif" />
+    <meta property="fc:frame:image" content="https://bookies-frames.vercel.app/${buttonIndex === 1 ? "49ers.gif" : "chiefs.gif"}" />
   </head></html>`);
 }
 
