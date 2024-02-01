6819b96eb058d3b31a859f10c40cea892f51701e
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-01-31 18:39:36 -0800

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   // let accountAddress: string | undefined = '';
-  const { body } = await req.json();
-  // const { isValid, message } = await getFrameMessage(body);
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body);
 
   // if (isValid) {
   //   try {
@@ -14,11 +14,11 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   //   }
   // }
 
-  // const buttonIndex = body.buttonIndex as number;
+  const buttonIndex = message.data.buttonIndex as number;
 
   return new NextResponse(`<!DOCTYPE html><html><head>
     <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="https://bookies-frames.vercel.app/${true ? "49ers.gif" : "chiefs.gif"}" />
+    <meta property="fc:frame:image" content="https://bookies-frames.vercel.app/${buttonIndex === 1 ? "49ers.gif" : "chiefs.gif"}" />
   </head></html>`);
 }
 
