e8a5d3e8c7bb53d2f8e2aa3d926a2e3c67c16f51
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-04 11:45:20 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,4 @@
-import { getFrameMetadata } from '@coinbase/onchainkit';
-import type { Metadata } from 'next';
 import { redirect } from 'next/navigation';
-
-const frameMetadata = getFrameMetadata({
-  buttons: [
-    {
-      label: 'Chiefs',
-    },
-    {
-      label: '49ers',
-    },
-  ],
-  image: `${process.env['HOST']}/superbowl.png`,
-  post_url: `${process.env['HOST']}/api/frames/sblviii-winner/vote`,
-});
-
-export const metadata: Metadata = {
-  title: 'Superbowl LVIII Poll',
-  description: 'The first non-custiodal sports betting exchange with the vision of democratizing the sports betting experience.',
-  openGraph: {
-    title: 'Superbowl LVIII Poll',
-    description: 'The first non-custiodal sports betting exchange with the vision of democratizing the sports betting experience.', 
-    images: [`${process.env['HOST']}/superbowl.png`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
 
 export default function Page() {
   redirect('https://bookies-rho.vercel.app/');
