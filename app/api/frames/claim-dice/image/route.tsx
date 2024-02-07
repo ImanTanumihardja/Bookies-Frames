f473372e03de3d655473f92d0e48b0f6a7d1abf0
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-07 12:09:24 -0800

    Update route.tsx

--- app/api/frames/claim-dice/image/route.tsx
+++ app/api/frames/claim-dice/image/route.tsx
@@ -1,4 +1,5 @@
-import { ImageResponse, NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+import { ImageResponse } from 'next/og';
 import FrameBase from '../../../../../src/components/FrameBase'
 import { RequestProps, getRequestProps } from '../../../../../src/utils';
 
