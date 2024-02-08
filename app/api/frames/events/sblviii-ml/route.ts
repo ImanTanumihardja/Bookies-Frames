bd5af9f194e1ede84b6ea73ad6ac87d92104f8b7
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-07 23:54:07 -0800

    Update route.ts

--- app/api/frames/events/sblviii-ml/route.ts
+++ app/api/frames/events/sblviii-ml/route.ts
@@ -14,59 +14,59 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     const fid: number = message?.interactor.fid || 0;
     const accountAddress: string = message?.interactor.custody_address || "";
     const frameName: string = req.nextUrl.pathname.split('/').pop() || "";
-    let user : User = await kv.hgetall(accountAddress) || {} as User
+    // let user : User = await kv.hgetall(accountAddress) || {} as User
 
-    // Check if I can parse the amount as integer
-    let wagerAmount;
-    try {
-      wagerAmount = parseInt(message?.input || "0");
-    }
-    catch (e) {
-      return new NextResponse(
-        // Return a response with a error message
-        getFrameHtmlResponse({
-          image: `${process.env['HOST']}/superbowl.png`,
-        }),
-      );
-    }
+    // // Check if I can parse the amount as integer
+    // let wagerAmount;
+    // try {
+    //   wagerAmount = parseInt(message?.input || "0");
+    // }
+    // catch (e) {
+    //   return new NextResponse(
+    //     // Return a response with a error message
+    //     getFrameHtmlResponse({
+    //       image: `${process.env['HOST']}/superbowl.png`,
+    //     }),
+    //   );
+    // }
 
-    // Check if the amount is valid
-    if (wagerAmount > user.points) {
-      return new NextResponse(
-        // Return a response with a error message
-        getFrameHtmlResponse({
-          image: `${process.env['HOST']}/superbowl.png`,
-        }),
-      );
-    }
+    // // Check if the amount is valid
+    // if (wagerAmount > user.points) {
+    //   return new NextResponse(
+    //     // Return a response with a error message
+    //     getFrameHtmlResponse({
+    //       image: `${process.env['HOST']}/superbowl.png`,
+    //     }),
+    //   );
+    // }
 
-    // Get the poll data from database or init if not exists
-    let event : Event = await kv.hgetall(frameName) || {startDate: 1707694200000, poll: [0, 0], bets: {} as Record<number, Bet>, result: -1};
+    // // Get the poll data from database or init if not exists
+    // let event : Event = await kv.hgetall(frameName) || {startDate: 1707694200000, poll: [0, 0], bets: {} as Record<number, Bet>, result: -1};
 
-    const now = new Date().getTime();
+    // const now = new Date().getTime();
 
-    // Check if voted before and if the event is closed
-    const voteExists = event?.bets.hasOwnProperty(fid);
-    if (!voteExists && now < event?.startDate) {
-      const multi = kv.multi();
+    // // Check if voted before and if the event is closed
+    // const voteExists = event?.bets.hasOwnProperty(fid);
+    // if (!voteExists && now < event?.startDate) {
+    //   const multi = kv.multi();
 
-      event.poll[prediction]++;
-      event.bets[fid] = {eventName: frameName, wagerAmount: wagerAmount, prediction:prediction, timeStamp: now};
-      await multi.hset(frameName, event);
+    //   event.poll[prediction]++;
+    //   event.bets[fid] = {eventName: frameName, wagerAmount: wagerAmount, prediction:prediction, timeStamp: now};
+    //   await multi.hset(frameName, event);
 
-      user.points -= wagerAmount;
-      await multi.hset(accountAddress, user);
+    //   user.points -= wagerAmount;
+    //   await multi.hset(accountAddress, user);
 
-      await multi.exec();
-    } 
-    else if (event.startDate >= now) {
-      prediction = event.bets[fid].prediction;
-    }
-    else {
-      prediction = -1
-    }
+    //   await multi.exec();
+    // } 
+    // else if (event.startDate >= now) {
+    //   prediction = event.bets[fid].prediction;
+    // }
+    // else {
+    //   prediction = -1
+    // }
 
-    const imageUrl = `${process.env['HOST']}/api/frames/${frameName}/image?chiefs=${event.poll[0]}&niners=${event.poll[1]}&result=${event.result}&prediction=${prediction}&timestamp=${now}`;
+    const imageUrl = `${process.env['HOST']}/superbowl.png`//`${process.env['HOST']}/api/frames/${frameName}/image?chiefs=${event.poll[0]}&niners=${event.poll[1]}&result=${event.result}&prediction=${prediction}&timestamp=${now}`;
 
     return new NextResponse(
       getFrameHtmlResponse({
