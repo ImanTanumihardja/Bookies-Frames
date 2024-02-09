b8ec7ef15a6f66985222c20686ec0e47f7a89671
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-08 22:53:40 -0800

    Update route.tsx

--- app/api/frames/profile-finder/image/route.tsx
+++ app/api/frames/profile-finder/image/route.tsx
@@ -29,11 +29,13 @@ export async function GET(req: NextRequest) {
         return new ImageResponse(
             <FrameBase>
                 {isFollowing ?
-                    rank !== -1?
+                    rank === -1 && !username  ?
+                    <h2 style={{color: 'white', fontSize:40, textAlign:'center'}}> No profile found</h2>
+                    :
                     <div style={{display: 'flex', flexDirection: 'column', width:'100%'}}>
                         <h1 style={{color: 'white', alignItems:'center', left:-30}}> 
                             {avatarUrl && <img style={{width: 40, height: 40, marginRight:10, borderRadius: 50}} src={avatarUrl}/>} 
-                            {username || ""} (#{rank})
+                            username {rank ? `(#${rank})` : ''}
                         </h1>
                         <div style={{ display: 'flex', flexDirection: 'column', alignItems:'flex-start', alignSelf:'center', top:-30}}>
                             <h2 style={{color: 'white', marginBottom:-10}}> 🎲 Dice: {points} </h2>
@@ -41,8 +43,6 @@ export async function GET(req: NextRequest) {
                             <h2 style={{color: 'white'}}> 🎰 Total Bets: {numBets} ({wins}W - {losses}L)</h2>
                         </div>
                     </div>
-                    :
-                    <h2 style={{color: 'white', fontSize:40, textAlign:'center'}}> No profile found</h2>
                     :
                     <NotFollowing/>
                 }
