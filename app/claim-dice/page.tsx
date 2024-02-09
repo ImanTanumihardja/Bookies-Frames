00b8385363515365b27d32659dd6aa827eb44aa6
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-08 17:25:04 -0800

    Update page.tsx

--- app/claim-dice/page.tsx
+++ app/claim-dice/page.tsx
@@ -10,7 +10,7 @@ const frameMetadata = getFrameMetadata({
       label: 'Claim free Dice!',
     },
   ],
-  image: `${process.env['HOST']}/thumbnail/${frameName}.gif`,
+  image: `${process.env['HOST']}/thumbnails/${frameName}.gif`,
   post_url: `${process.env['HOST']}/api/frames/${frameName}`
 });
 
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: frameName,
     description: 'Claim 100 free Dice if you follow!', 
-    images: [`${process.env['HOST']}/thumbnail/${frameName}.gif`],
+    images: [`${process.env['HOST']}/thumbnails/${frameName}.gif`],
   },
   other: {
     ...frameMetadata,
