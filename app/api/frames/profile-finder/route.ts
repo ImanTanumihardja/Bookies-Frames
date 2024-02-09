ca57a1c6f0857171ec26e87fa866972ffaa1f65e
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-09 10:52:11 -0800

    Update route.ts

--- app/api/frames/profile-finder/route.ts
+++ app/api/frames/profile-finder/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { Frame, getFrameHtml } from "frames.js";
-import { DEFAULT_USER, generateImageUrl, RequestProps, validateFrameMessage, neynarClient } from '../../../../src/utils';
+import { DEFAULT_USER, generateImageUrl, RequestProps, validateFrameMessage, neynarClient, BOOKIES_FID } from '../../../../src/utils';
 import { User } from '../../../types';
 import { kv } from '@vercel/kv';
 
@@ -21,8 +21,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   let user : User = DEFAULT_USER;
   let rank : number = -1;
 
-  await neynarClient.lookupUserByUsername(username).then( (res) => {
-    profile = res.result?.user;
+  await neynarClient.searchUser(username, BOOKIES_FID).then( (res) => {
+    profile = res.result?.users[0]; // Grap first user
     fid = profile?.fid;
   })
   .catch ( (error) => {
@@ -31,7 +31,6 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   })
   .finally(async () => {
       if (profile !== null) {
-
         rank = await kv.zrank('users', profile?.fid || "") || -1
         
         // Can skip if not found in db
