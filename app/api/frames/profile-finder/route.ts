0b22d4b175ca07115a1939142905ae8c0fe104dd
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-09 14:43:29 -0800

    Update route.ts

--- app/api/frames/profile-finder/route.ts
+++ app/api/frames/profile-finder/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest): Promise<Response> {
 
     // Submit post request to /profile-finder/search
     if (button === 2) {
-      return await fetch(`${process.env['HOST']}/api/frames/${frameName}}/search`, {method: 'POST', body: req.body, headers: {'Content-Type': 'application/json'});
+      return await fetch(`${process.env['HOST']}/api/frames/${frameName}}/search`, {method: 'POST', body: req.body, headers: {'Content-Type': 'application/json'}});
     }
     // Submit post request to /profile-finder/profile-page
     else if (button === 1) {
