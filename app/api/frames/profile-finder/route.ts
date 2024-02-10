f01eb0dd3207da66a13133f1ba5a2c888aacae20
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-09 17:00:04 -0800

    test

--- app/api/frames/profile-finder/route.ts
+++ app/api/frames/profile-finder/route.ts
@@ -5,57 +5,57 @@ import { User } from '../../../types';
 import { kv } from '@vercel/kv';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  // // Verify the frame request
-  // const message = await validateFrameMessage(req);
+  // Verify the frame request
+  const message = await validateFrameMessage(req);
 
-  // const {followingBookies: isFollowing } = message;
+  const {followingBookies: isFollowing } = message;
 
-  // const frameName: string = req.nextUrl.pathname.split('/').pop() || "";
+  const frameName: string = req.nextUrl.pathname.split('/').pop() || "";
 
-  // // Check for fid prop in url and if there use that as fid
-  // const username : string = (req.nextUrl.searchParams.get("username") || message?.input).toLowerCase();
+  // Check for fid prop in url and if there use that as fid
+  const username : string = (req.nextUrl.searchParams.get("username") || message?.input).toLowerCase();
 
-  // if (!username || username === "") {
-  //   return new NextResponse('No username provided', { status: 400 });
-  // }
+  if (!username || username === "") {
+    return new NextResponse('No username provided', { status: 400 });
+  }
 
-  // let profile: any = null;
-  // let imageUrl: string = "";
-  // let user : User = DEFAULT_USER;
-  // let rank : number = -1;
+  let profile: any = null;
+  let imageUrl: string = "";
+  let user : User = DEFAULT_USER;
+  let rank : number = -1;
 
-  // await neynarClient.searchUser(username, BOOKIES_FID).then( (res) => {
-  //   const users = res?.result?.users;
-  //   profile = users?.length > 0 ? users[0] : null;
-  // })
-  // .catch ( (error) => {
-  //   console.error(error);
-  //   profile = null;
-  // })
-  // .finally(async () => {
-  //     if (profile !== null) {
-  //       rank = await kv.zrank('users', profile?.fid || "") || -1
+  await neynarClient.searchUser(username, BOOKIES_FID).then( (res) => {
+    const users = res?.result?.users;
+    profile = users?.length > 0 ? users[0] : null;
+  })
+  .catch ( (error) => {
+    console.error(error);
+    profile = null;
+  })
+  .finally(async () => {
+      if (profile !== null) {
+        rank = await kv.zrank('users', profile?.fid || "") || -1
         
-  //       // Can skip if not found in kv
-  //       if (rank !== -1) user = await kv.hgetall(profile?.fid?.toString() || "") || DEFAULT_USER;
-  //     }
+        // Can skip if not found in kv
+        if (rank !== -1) user = await kv.hgetall(profile?.fid?.toString() || "") || DEFAULT_USER;
+      }
   
-  //   imageUrl = generateImageUrl(frameName, {[RequestProps.IS_FOLLOWING]: isFollowing, 
-  //                                           [RequestProps.USERNAME]: profile?.username || "", 
-  //                                           [RequestProps.AVATAR_URL]: profile?.pfp.url || "", 
-  //                                           [RequestProps.RANK]: rank, 
-  //                                           [RequestProps.WINS]: user.wins, 
-  //                                           [RequestProps.LOSSES]: user.losses, 
-  //                                           [RequestProps.POINTS]: user.points, 
-  //                                           [RequestProps.STREAK]: user.streak, 
-  //                                           [RequestProps.NUM_BETS]: user.numBets});
-  //  });
+    imageUrl = generateImageUrl(frameName, {[RequestProps.IS_FOLLOWING]: isFollowing, 
+                                            [RequestProps.USERNAME]: profile?.username || "", 
+                                            [RequestProps.AVATAR_URL]: profile?.pfp.url || "", 
+                                            [RequestProps.RANK]: rank, 
+                                            [RequestProps.WINS]: user.wins, 
+                                            [RequestProps.LOSSES]: user.losses, 
+                                            [RequestProps.POINTS]: user.points, 
+                                            [RequestProps.STREAK]: user.streak, 
+                                            [RequestProps.NUM_BETS]: user.numBets});
+   });
 
   const frame: Frame = {
     version: "vNext",
-    image: `${process.env['HOST']}/thumbnails/profile-finder.gif`,
+    image: imageUrl,
     buttons: [{label: "Back", action: "post"}],
-    postUrl: `${process.env['HOST']}/api/frames/`,
+    postUrl: `${process.env['HOST']}/api/frames/${frameName}`,
   };
 
   return new NextResponse(
---
+++ src/post_methods/profile-finder.ts
@@ -0,0 +1,68 @@
+// import { NextRequest, NextResponse } from 'next/server';
+// import { validateFrameMessage, neynarClient, BOOKIES_FID, DEFAULT_USER, RequestProps, generateImageUrl } from '../../../../src/utils';
+// import { User } from '../../../types';
+// import { kv } from '@vercel/kv';
+// import { Frame, getFrameHtml } from 'frames.js';
+
+
+// async function getProfileFinderResponse(req: NextRequest): Promise<NextResponse> {
+//   // Verify the frame request
+//   const message = await validateFrameMessage(req);
+
+//   const {followingBookies: isFollowing } = message;
+
+//   const frameName: string = req.nextUrl.pathname.split('/').pop() || "";
+
+//   // Check for fid prop in url and if there use that as fid
+//   const username : string = (req.nextUrl.searchParams.get("username") || message?.input).toLowerCase();
+
+//   if (!username || username === "") {
+//     return new NextResponse('No username provided', { status: 400 });
+//   }
+
+//   let profile: any = null;
+//   let imageUrl: string = "";
+//   let user : User = DEFAULT_USER;
+//   let rank : number = -1;
+
+//   await neynarClient.searchUser(username, BOOKIES_FID).then( (res) => {
+//     const users = res?.result?.users;
+//     profile = users?.length > 0 ? users[0] : null;
+//   })
+//   .catch ( (error) => {
+//     console.error(error);
+//     profile = null;
+//   })
+//   .finally(async () => {
+//       if (profile !== null) {
+//         rank = await kv.zrank('users', profile?.fid || "") || -1
+        
+//         // Can skip if not found in kv
+//         if (rank !== -1) user = await kv.hgetall(profile?.fid?.toString() || "") || DEFAULT_USER;
+//       }
+  
+//     imageUrl = generateImageUrl(frameName, {[RequestProps.IS_FOLLOWING]: isFollowing, 
+//                                             [RequestProps.USERNAME]: profile?.username || "", 
+//                                             [RequestProps.AVATAR_URL]: profile?.pfp.url || "", 
+//                                             [RequestProps.RANK]: rank, 
+//                                             [RequestProps.WINS]: user.wins, 
+//                                             [RequestProps.LOSSES]: user.losses, 
+//                                             [RequestProps.POINTS]: user.points, 
+//                                             [RequestProps.STREAK]: user.streak, 
+//                                             [RequestProps.NUM_BETS]: user.numBets});
+//    });
+
+//   const frame: Frame = {
+//     version: "vNext",
+//     image: imageUrl,
+//     buttons: [{label: "Back", action: "post"}],
+//     postUrl: `${process.env['HOST']}/api/frames/${frameName}`,
+//   };
+
+//   return new NextResponse(
+//     getFrameHtml(frame),
+//   );
+// }
+
+
+// export const dynamic = 'force-dynamic';
