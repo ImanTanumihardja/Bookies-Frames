580de5888369a4ce599216923acc2e990a4ccb74
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-11 14:20:39 -0800

    Update route.ts

--- app/api/frames/claim-dice/route.ts
+++ app/api/frames/claim-dice/route.ts
@@ -23,6 +23,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
 
   if (isFollowing && validCaptcha) {
     user = await kv.hgetall(fid.toString()) || DEFAULT_USER;
+    console.log(fid)
     hasClaimed = user.hasClaimed;
     console.log(user)
     if (!hasClaimed) {
