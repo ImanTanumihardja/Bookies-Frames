c4207c8a0782c778c27fc44294ac368cd2c71599
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-11 14:25:39 -0800

    Update route.ts

--- app/api/frames/claim-dice/route.ts
+++ app/api/frames/claim-dice/route.ts
@@ -4,8 +4,10 @@ import { User} from '../../../types';
 import { RequestProps, generateUrl, DEFAULT_USER, validateFrameMessage } from '../../../../src/utils';
 import { getFrameHtml, Frame} from "frames.js";
 import { FrameNames } from '../../../../src/utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
+  noStore();
   
   // Verify the frame request
   const message = await validateFrameMessage(req);
