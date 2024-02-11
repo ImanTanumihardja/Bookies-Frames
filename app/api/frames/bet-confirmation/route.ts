c3969b527b66dbbe780dbf8d0b05bda6526e7873
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-11 15:39:43 -0800

    Staged

--- app/api/frames/bet-confirmation/route.ts
+++ app/api/frames/bet-confirmation/route.ts
@@ -73,3 +73,4 @@ export async function POST(req: NextRequest): Promise<Response> {
 
 export const revalidate = 0;
 export const dynamic = 'force-dynamic';
+export const fetchCache = 'force-no-store';
