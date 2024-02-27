37f561e0b8e2fe36fd3ae6fc91c657944bc1a0cb
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-27 08:36:02 -0800

    Update page.tsx

--- app/events/luton-city-spread/page.tsx
+++ app/events/luton-city-spread/page.tsx
@@ -1,47 +1,48 @@
-import type { Metadata } from 'next';
+import type { Metadata, ResolvingMetadata } from 'next';
 import { redirect } from 'next/navigation';
 import { FrameNames, RequestProps, generateUrl } from '../../../src/utils';
 import { Frame, getFrameFlattened } from 'frames.js';
 import { Props } from 'next/script';
 
-const options = ["Luton +1.5", "City -1.5"]
-const imageUrl = generateUrl(`api/frames/${FrameNames.EVENT_THUMBNAIL}/image`, {[RequestProps.EVENT_NAME]: FrameNames.LUTON_CITY_SPREAD}, true)
 
-const frame : Frame = {
-  version: "vNext",
-  inputText: 'Amount of dice you want to bet',
+export async function generateMetadata(
+  {  }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const options = ["Luton +1.5", "City -1.5"]
+  const imageUrl = generateUrl(`api/frames/${FrameNames.EVENT_THUMBNAIL}/image`, {[RequestProps.EVENT_NAME]: FrameNames.LUTON_CITY_SPREAD}, true)
 
-  buttons: [
-    {
-      label: options[0],
-      action: 'post',
-    },
-    {
-      label: options[1],
-      action: 'post',
-    },
-  ],
-  image: imageUrl,
-  postUrl: `${process.env['HOST']}/api/frames/${FrameNames.BETSLIP}?eventName=${FrameNames.LUTON_CITY_SPREAD}`
-};
+  const frame : Frame = {
+    version: "vNext",
+    inputText: 'Amount of dice you want to bet',
 
-// export async function generateMetadata(
-//   { params, searchParams }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
+    buttons: [
+      {
+        label: options[0],
+        action: 'post',
+      },
+      {
+        label: options[1],
+        action: 'post',
+      },
+    ],
+    image: imageUrl,
+    postUrl: `${process.env['HOST']}/api/frames/${FrameNames.BETSLIP}?eventName=${FrameNames.LUTON_CITY_SPREAD}`
+  };
 
-// }
-
-export const metadata: Metadata = {
-  title: FrameNames.LUTON_CITY_SPREAD,
-  description: 'Will Man City win by more or less than 1.5 goals?',
-  openGraph: {
+  const metadata: Metadata = {
     title: FrameNames.LUTON_CITY_SPREAD,
-    description: 'Will Man City win by more or less than 1.5 goals?', 
-    images: [imageUrl],
-  },
-  other: getFrameFlattened(frame),
-};
+    description: 'Will Man City win by more or less than 1.5 goals?',
+    openGraph: {
+      title: FrameNames.LUTON_CITY_SPREAD,
+      description: 'Will Man City win by more or less than 1.5 goals?', 
+      images: [imageUrl],
+    },
+    other: getFrameFlattened(frame),
+  };
+
+  return metadata
+}
 
 export default async function Page() {
   // redirect('https://bookies-rho.vercel.app/');
