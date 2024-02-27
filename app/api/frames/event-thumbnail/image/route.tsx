fd5d897e12afbae270086ba430b0e631115c5ff7
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-02-27 12:24:05 -0800

    Update route.tsx

--- app/api/frames/event-thumbnail/image/route.tsx
+++ app/api/frames/event-thumbnail/image/route.tsx
@@ -7,12 +7,12 @@ import satori from "satori";
 import sharp from 'sharp';
 
 // Fonts
-const plusJakartaSans = fetch(
-    new URL(
-      '@fontsource/plus-jakarta-sans/files/plus-jakarta-sans-latin-700-normal.woff',
-      import.meta.url,
-    ),
-  ).then((res) => res.arrayBuffer());
+// const plusJakartaSans = fetch(
+//     new URL(
+//       '@fontsource/plus-jakarta-sans/files/plus-jakarta-sans-latin-700-normal.woff',
+//       import.meta.url,
+//     ),
+//   ).then((res) => res.arrayBuffer());
 
 export async function GET(req: NextRequest) {
     try {
@@ -38,7 +38,7 @@ export async function GET(req: NextRequest) {
             {
                 width: 764, 
                 height: 400, 
-                fonts: [{ name: 'Plus_Jakarta_Sans_700', data: await plusJakartaSans, weight: 400 }],
+                fonts: [],
             }
         )
 
@@ -78,7 +78,7 @@ export async function GET(req: NextRequest) {
     }
 }
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 export const revalidate = 0;
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
