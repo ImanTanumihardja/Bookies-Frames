793cced061afc99d447d8796295e10f14ac21d29
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-03-06 17:35:57 -0800

    Upadte

--- app/api/frames/captcha/image/route.tsx
+++ app/api/frames/captcha/image/route.tsx
@@ -3,12 +3,12 @@ import { ImageResponse } from 'next/og';
 import FrameBase from '../../../../../src/components/FrameBase'
 
 // Fonts
-const plusJakartaSans = fetch(
-    new URL(
-      '@fontsource/plus-jakarta-sans/files/plus-jakarta-sans-latin-700-normal.woff',
-      import.meta.url,
-    ),
-  ).then((res) => res.arrayBuffer());
+// const plusJakartaSans = fetch(
+//     new URL(
+//       '@fontsource/plus-jakarta-sans/files/plus-jakarta-sans-latin-700-normal.woff',
+//       import.meta.url,
+//     ),
+//   ).then((res) => res.arrayBuffer());
 
 export async function GET(req: NextRequest) {
     try {
@@ -25,7 +25,7 @@ export async function GET(req: NextRequest) {
             {
                 width: 764, 
                 height: 400, 
-                fonts: [{ name: 'Plus_Jakarta_Sans_700', data: await plusJakartaSans, weight: 400 }],
+                // fonts: [{ name: 'Plus_Jakarta_Sans_700', data: await plusJakartaSans, weight: 400 }],
                 headers:{
                     'CDN-Cache-Control': 'public, s-maxage=60',
                     'Vercel-CDN-Cache-Control': 'public, s-maxage=60'
