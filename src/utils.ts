51a61e046b89cee043db16dc1057354814962ec4
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-03-06 21:39:51 -0800

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server'
 import { NeynarAPIClient } from "@neynar/nodejs-sdk";
 import { User, Bet} from '../app/types';
 import { FrameValidationData } from '../app/types';
-import { getFrameHtml, getFrameMessage as validateFrameMessage } from 'frames.js';
+const { getFrameHtml, getFrameMessage: validateFrameMessage } = require('frames.js');
 
 
 export enum RequestProps {
