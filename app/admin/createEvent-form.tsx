a21520996da1fc6d5cf014c2b6cc4e6f97254510
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-03-07 12:34:00 -0800

    Update createEvent-form.tsx

--- app/admin/createEvent-form.tsx
+++ app/admin/createEvent-form.tsx
@@ -28,17 +28,17 @@ export function CreateEventForm() {
       <h1>Create Event</h1>
       <form action={formAction}>
         <label htmlFor="eventName">Event Name</label>
-        <input type="text" id="eventName" name="eventName" required />
-        <label htmlFor="startDate">Start Date</label>
-        <input type="number" id="startDate" name="startDate" required />
+        <input type="text" id="eventName" name="eventName" placeholder="lal-lac-ml" required />
+        <label htmlFor="startDate">Start Date (Unix)</label>
+        <input type="number" id="startDate" name="startDate" placeholder="1709777045000" required />
         <label htmlFor="odds">Odds</label>
-        <input type="text" id="odds" name="odds" required />
+        <input type="text" id="odds" name="odds" placeholder="0.5, 0.5" required />
         <label htmlFor="multiplier">Multiplier</label>
-        <input type="number" id="multiplier" name="multiplier" required />
+        <input type="number" id="multiplier" name="multiplier" placeholder='1' required />
         <label htmlFor="options">Options</label>
-        <input type="text" id="options" name="options" required />
+        <input type="text" id="options" name="options" placeholder="LAL, LAC" required />
         <label htmlFor="prompt">Prompt</label>
-        <input type="text" id="prompt" name="prompt" required />
+        <input type="text" id="prompt" name="prompt" placeholder="Lakers vs Clippers" required />
         <SubmitButton />
 
         <CldUploadWidget uploadPreset="thumbnails">
