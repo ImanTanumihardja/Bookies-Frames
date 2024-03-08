51a6b69ba570fe792db79db852ae4e3f1bc11ae6
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-03-08 12:54:43 -0800

    Update route.ts

--- app/api/send-ether/route.ts
+++ app/api/send-ether/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { generateUrl, getFrameMessage } from '../../../src/utils';
 import { Frame, getFrameHtml} from "frames.js";
-import ethers from 'ethers';
+import {parseEther} from 'ethers';
 
 export async function POST(req: NextRequest): Promise<Response> {
   // Verify the frame request
   const message = await getFrameMessage(req, true);
-
-  
 
   const txData = {
       chainId: `eip155:8453`,
@@ -16,7 +14,7 @@ export async function POST(req: NextRequest): Promise<Response> {
         abi: [],
         // data
         to: '0xB97331E86EA4cE364491650e127e096C720dc422',
-        value: ethers.parseEther('0.0001').toString(), // 0.0001 ETH
+        value: parseEther('0.0001').toString(), // 0.0001 ETH
       },
     };
     return NextResponse.json(txData);
