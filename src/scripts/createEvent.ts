e4e43e56ff9041f3695649a211abf811a7b2542b
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-05-07 10:18:36 -0700

    Update createEvent.ts

--- src/scripts/createEvent.ts
+++ src/scripts/createEvent.ts
@@ -69,106 +69,106 @@ export default async function createEvent(eventName=``, startDate=0, odds=[0.5, 
 
         console.log('Ancillary Data: ', ancillaryData)
 
-        // const tx = await orderBookiefactory.createOrderBookie(ethers.toUtf8Bytes(JSON.stringify(ancillaryData)),
-        //                                                       startDate, // Convert from milli-seconds to seconds
-        //                                                       0,
-        //                                                       0,
-        //                                                       1800,
-        //                                                       USDC_ADDRESS,
-        //                                                       acceptedToken,
-        //                                                       ethers.encodeBytes32String("MULTIPLE_CHOICE_QUERY"),
-        //                                                       signerAddress,
-        //                                                       false)
+        const tx = await orderBookiefactory.createOrderBookie(ethers.toUtf8Bytes(JSON.stringify(ancillaryData)),
+                                                              startDate, // Convert from milli-seconds to seconds
+                                                              0,
+                                                              0,
+                                                              1800,
+                                                              USDC_ADDRESS,
+                                                              acceptedToken,
+                                                              ethers.encodeBytes32String("MULTIPLE_CHOICE_QUERY"),
+                                                              signerAddress,
+                                                              false)
 
-        // // Get address of create contract
-        // const txReceipt = await tx.wait()
+        // Get address of create contract
+        const txReceipt = await tx.wait()
 
-        // console.log('TX RECEIPT: ', txReceipt.hash)
+        console.log('TX RECEIPT: ', txReceipt.hash)
         
-        // const logs:any = txReceipt?.logs.map((log:any) => orderBookiefactory.interface.parseLog(log))
+        const logs:any = txReceipt?.logs.map((log:any) => orderBookiefactory.interface.parseLog(log))
 
-        // // Find event with Name OrderBookieEvent
-        // const event = logs?.find((log:any) => log?.name === "OrderBookieCreated")
+        // Find event with Name OrderBookieEvent
+        const event = logs?.find((log:any) => log?.name === "OrderBookieCreated")
 
-        // // Print the arg
-        // console.log('OrderBookie Address: ', event?.args[0])
-        // address = event?.args[0]
+        // Print the arg
+        console.log('OrderBookie Address: ', event?.args[0])
+        address = event?.args[0]
 
-        // Verify the contract on etherscan
-        const instance = new Etherscan(
-          process.env.BASESCAN_API_KEY || '', // Etherscan API key
-          "https://api.basescan.org/api", // Etherscan API URL
-          "https://basescan.org/" // Etherscan browser URL
-        );
+        // // Verify the contract on etherscan
+        // const instance = new Etherscan(
+        //   process.env.BASESCAN_API_KEY || '', // Etherscan API key
+        //   "https://api.basescan.org/api", // Etherscan API URL
+        //   "https://basescan.org/" // Etherscan browser URL
+        // );
 
-        // Read in json file
-        const contractSourceCode = JSON.parse(fs.readFileSync(process.cwd() + '/app/json/orderbookieVerify.json', 'utf8'));
+        // // Read in json file
+        // const contractSourceCode = JSON.parse(fs.readFileSync(process.cwd() + '/app/json/orderbookieVerify.json', 'utf8'));
 
-        // Encode parameters using orderbookie interface
-        const encodedConstructorArgs = orderBookiefactory.interface.encodeFunctionData("createOrderBookie", [
-          ethers.toUtf8Bytes(JSON.stringify(ancillaryData)),
-          startDate, // Convert from milli-seconds to seconds
-          0,
-          0,
-          1800,
-          USDC_ADDRESS,
-          acceptedToken,
-          ethers.encodeBytes32String("MULTIPLE_CHOICE_QUERY"),
-          signerAddress,
-          false
-        ]);
+        // // Encode parameters using orderbookie interface
+        // const encodedConstructorArgs = orderBookiefactory.interface.encodeFunctionData("createOrderBookie", [
+        //   ethers.toUtf8Bytes(JSON.stringify(ancillaryData)),
+        //   startDate, // Convert from milli-seconds to seconds
+        //   0,
+        //   0,
+        //   1800,
+        //   USDC_ADDRESS,
+        //   acceptedToken,
+        //   ethers.encodeBytes32String("MULTIPLE_CHOICE_QUERY"),
+        //   signerAddress,
+        //   false
+        // ]);
 
-        if (!instance.isVerified(address)) {
-          const { message: guid } = await instance.verify(
-            // Contract address
-            address,
-            // Contract source code
-            contractSourceCode.stringify(),
-            // Contract name
-            "contracts/OrderBookie.sol:OrderBookie",
-            // Compiler version
-            "v0.8.19+commit.7dd6d404",
-            // Encoded constructor arguments
-            encodedConstructorArgs
-          );
+        // if (!instance.isVerified(address)) {
+        //   const { message: guid } = await instance.verify(
+        //     // Contract address
+        //     address,
+        //     // Contract source code
+        //     contractSourceCode.stringify(),
+        //     // Contract name
+        //     "contracts/OrderBookie.sol:OrderBookie",
+        //     // Compiler version
+        //     "v0.8.19+commit.7dd6d404",
+        //     // Encoded constructor arguments
+        //     encodedConstructorArgs
+        //   );
         
-          await sleep(1000);
-          const verificationStatus = await instance.getVerificationStatus(guid);
+        //   await sleep(1000);
+        //   const verificationStatus = await instance.getVerificationStatus(guid);
         
-          if (verificationStatus.isSuccess()) {
-            const contractURL = instance.getContractUrl(address);
-            console.log(
-              `Successfully verified contract "MyContract" on Etherscan: ${contractURL}`
-            );
-          }
-        }
+        //   if (verificationStatus.isSuccess()) {
+        //     const contractURL = instance.getContractUrl(address);
+        //     console.log(
+        //       `Successfully verified contract "MyContract" on Etherscan: ${contractURL}`
+        //     );
+        //   }
+        // }
     }
     else {  
       throw new Error(`Ancillary data is required for bookies`)
     }
   }
 
-  // let event: Event = {startDate, result: -1, odds, options, prompt, host, address} as Event;
-  // await kv.hset(`${eventName}`, event);
+  let event: Event = {startDate, result: -1, odds, options, prompt, host, address} as Event;
+  await kv.hset(`${eventName}`, event);
 
-  // // Create poll
-  // const poll = {0: 0, 1: 0, 2: 0, 3: 0} as Record<number, number>
-  // await kv.hset(`${eventName}:${DatabaseKeys.POLL}`, poll)
+  // Create poll
+  const poll = {0: 0, 1: 0, 2: 0, 3: 0} as Record<number, number>
+  await kv.hset(`${eventName}:${DatabaseKeys.POLL}`, poll)
 
-  // if (host === Accounts.ALEA || host === Accounts.BOTH) {
-  //   // Create alea bets list 
-  //   await kv.del(`${Accounts.ALEA}:${eventName}:${DatabaseKeys.BETTORS}`)
-  // }
+  if (host === Accounts.ALEA || host === Accounts.BOTH) {
+    // Create alea bets list 
+    await kv.del(`${Accounts.ALEA}:${eventName}:${DatabaseKeys.BETTORS}`)
+  }
 
-  // if (host === Accounts.BOOKIES || host === Accounts.BOTH) {
-  //   // Create bookies bets list 
-  //   await kv.del(`${Accounts.BOOKIES}:${eventName}:${DatabaseKeys.BETTORS}`)
-  // }
+  if (host === Accounts.BOOKIES || host === Accounts.BOTH) {
+    // Create bookies bets list 
+    await kv.del(`${Accounts.BOOKIES}:${eventName}:${DatabaseKeys.BETTORS}`)
+  }
 
-  // event = await kv.hgetall(`${eventName}`)
+  event = await kv.hgetall(`${eventName}`)
 
-  // console.log(`Event: ${eventName}`)
-  // console.log(event)
+  console.log(`Event: ${eventName}`)
+  console.log(event)
 }
 
 if (require.main === module) {
