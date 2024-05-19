1d66fa55cf8e110d8d464a6638e9190ba6117365
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-05-19 16:50:16 -0700

    Update route.ts

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,13 +1,19 @@
-export async function GET() {
-    const result = await fetch(
-      'http://worldtimeapi.org/api/timezone/America/Chicago',
-      {
-        cache: 'no-store',
-      },
-    );
-    const data = await result.json();
+import { kv } from "@vercel/kv";
+import { Accounts, DatabaseKeys } from "../../../src/utils";
+import { Event } from "../../types";
 
-    console.log(data)
+export async function GET() {
+    // Get bookies events 
+    const bookiesEvents = (await kv.sscan(`${Accounts.BOOKIES}:${DatabaseKeys.EVENTS}`, 0, {count: 150}))[1] as string[];
+
+    // Check each event is settled if so remove
+    for (const eventName of bookiesEvents) {
+        const eventInfo: Event | null = await kv.hgetall(eventName);
+        if (eventInfo && eventInfo.startDate < Date.now()) {
+            await kv.srem(`${Accounts.BOOKIES}:${DatabaseKeys.EVENTS}`, eventName);
+            console.log(`Removed event: ${eventName}`);
+        }
+    }
    
-    return Response.json({ datetime: data.datetime });
+    return Response.json({ message: 'Cron job ran successfully'});
   }
