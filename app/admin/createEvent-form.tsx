5fa7369c6e514d1e7b8d6f4d93fdb62f236661eb
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-05-23 19:16:14 -0700

    Update createEvent-form.tsx

--- app/admin/createEvent-form.tsx
+++ app/admin/createEvent-form.tsx
@@ -1,29 +1,33 @@
 'use client'
 
 import { useFormState, useFormStatus } from "react-dom";
-import {createEventAction} from "../actions"
+import { createEventAction } from "../actions";
 import { CldUploadWidget } from 'next-cloudinary';
 import { DEGEN_ADDRESS, USDC_ADDRESS } from "../json/addresses.json";
+import { ChangeEvent, useState } from 'react';
 
 function SubmitButton() {
   const { pending } = useFormStatus();
 
   return (
-    !pending &&
-    <button type="submit" aria-disabled={pending}>
-      Create
+    <button type="submit" disabled={pending} aria-disabled={pending}>
+      {pending ? "Creating..." : "Create"}
     </button>
   );
 }
 
 export function CreateEventForm() {
-  const [state, formAction] = useFormState(createEventAction, {message: ""});
+  const [state, formAction] = useFormState(createEventAction, { message: "" });
+  const [host, setHost] = useState("bookies");
 
-  if (state.message !== "") 
-  {
-    alert(state.message)
-    state.message = ""
+  if (state.message !== "") {
+    alert(state.message);
+    state.message = "";
   }
+
+  const handleHostChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setHost(e.target.value);
+  };
 
   return (
     <div>
@@ -50,33 +54,35 @@ export function CreateEventForm() {
         <br />
         <br />
         <label htmlFor="host">Host </label>
-        <select id="host" name="host">
-          <option value="bookies">Bookies</option>
+        <select id="host" name="host" value={host} onChange={handleHostChange}>
           <option value="alea">Alea</option>
+          <option value="bookies">Bookies</option>
           <option value="both">Both</option>
         </select>
         <br />
         <br />
-        <label htmlFor="description">Description (*bookies only*) </label>
-        <input type="text" id="description" name="description" />
-        <br />
-        <br />
-        <label htmlFor="acceptedToken">Accepted Token (*bookies only*) </label>
-        <select id="acceptedToken" name="acceptedToken">
-          <option value={`${USDC_ADDRESS}`}>USDC</option>
-          <option value={`${DEGEN_ADDRESS}`}>DEGEN</option>
-        </select>
-        <br />
-        <br />
+        {(host === 'bookies' || host === 'both') && (
+          <>
+            <label htmlFor="description">Description (*bookies only*) </label>
+            <input type="text" id="description" name="description" />
+            <br />
+            <br />
+            <label htmlFor="acceptedToken">Accepted Token (*bookies only*) </label>
+            <select id="acceptedToken" name="acceptedToken">
+              <option value={USDC_ADDRESS}>USDC</option>
+              <option value={DEGEN_ADDRESS}>DEGEN</option>
+            </select>
+            <br />
+            <br />
+          </>
+        )}
         <SubmitButton />
       </form>
-
       <br />
-
       <CldUploadWidget uploadPreset="thumbnails" signatureEndpoint={"../api/sign-image"}>
         {({ open }) => {
           return (
-            <button onClick={() => open()}>
+            <button type="button" onClick={() => open()}>
               Upload Thumbnail
             </button>
           );
