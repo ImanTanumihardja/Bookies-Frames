e167252066a4007d300d7b3ab4d8694e89154a6e
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-06-12 23:00:47 -0700

    Update cron

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,7 +1,1 @@
-import {createThirdwebClient, defineChain} from 'thirdweb';
-
-export const client = createThirdwebClient({
-    clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID as string,
-})
-
-export const myChain = defineChain(8453)
+// TODO move constants from utils file here
