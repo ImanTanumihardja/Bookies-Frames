866e0a52236c94bc695601112f8710e8a1b5c5a0
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-06-14 08:57:13 -0700

    Update

--- app/api/alea/[eventName]/image/route.tsx
+++ app/api/alea/[eventName]/image/route.tsx
@@ -12,15 +12,7 @@ let fontData = fs.readFileSync(fontPath)
 
 export async function GET(req: NextRequest, { params: { eventName } }: { params: { eventName: string } }) {
     try {
-        let startDate, prompt, creator;
-        try {
-            const {time, prompt: prompt_, fid} = getRequestProps(req, [RequestProps.TIME, RequestProps.PROMPT, RequestProps.FID]);
-            startDate = time;
-            prompt = prompt_;
-            creator = fid;
-        } catch (error) {
-            console.warn(error);
-        }
+        const {time: startDate} = getRequestProps(req, [RequestProps.TIME]);
 
         const now = new Date().getTime() / 1000;
 
@@ -28,9 +20,8 @@ export async function GET(req: NextRequest, { params: { eventName } }: { params:
         const till = startDate - now;
         const hours = Math.ceil(((till) / (60 * 60)) * 10) / 10;   
 
-        let imageUrl = null;
-        let profile;
-        let pfpURL;
+        let imageUrl ,profile, pfpURL, prompt = null;
+
         
 
         imageUrl = getCldImageUrl({
@@ -45,14 +36,13 @@ export async function GET(req: NextRequest, { params: { eventName } }: { params:
         if (!response.ok) {
             imageUrl = null;
             
+            const {prompt_, fid:creator} = getRequestProps(req, [RequestProps.PROMPT, RequestProps.FID]);
+            prompt = prompt_;
             if (prompt && creator)
             {
                 // Get creator profile from fid
                 profile = (await neynarClient.fetchBulkUsers([creator])).users[0];
                 pfpURL = `https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_jpg,w_168/${encodeURI(profile.pfp_url)}` 
-            }
-            else {
-                throw new Error('Could not generate thumbnail');
             }
         }
 
--- app/api/bookies/[eventName]/image/route.tsx
+++ app/api/bookies/[eventName]/image/route.tsx
@@ -12,15 +12,7 @@ let fontData = fs.readFileSync(fontPath)
 
 export async function GET(req: NextRequest, { params: { eventName } }: { params: { eventName: string } }) {
     try {
-        let startDate, prompt, creator;
-        try {
-            const {time, prompt: prompt_, fid} = getRequestProps(req, [RequestProps.TIME, RequestProps.PROMPT, RequestProps.FID]);
-            startDate = time;
-            prompt = prompt_;
-            creator = fid;
-        } catch (error) {
-            console.error(error);
-        }
+        const {time: startDate} = getRequestProps(req, [RequestProps.TIME]);
 
         const now = new Date().getTime() / 1000;
 
@@ -28,24 +20,25 @@ export async function GET(req: NextRequest, { params: { eventName } }: { params:
         const till = startDate - now;
         const hours = Math.ceil(((till) / (60 * 60)) * 10) / 10;
 
-        let imageUrl = null;
-        let profile = null;
-        let pfpURL = null;
+        let imageUrl, profile, pfpURL, prompt = null;
         
-        if (prompt && creator)
-        {
-            imageUrl = getCldImageUrl({
-                width: 960,
-                height: 600,
-                src: eventName
-            });
+        imageUrl = getCldImageUrl({
+            width: 960,
+            height: 600,
+            src: eventName
+        });
     
     
-            // Check if have thumbnail
-            const response = await fetch(imageUrl, { method: 'HEAD' });
-            if (!response.ok) {
-                imageUrl = null;
-                
+        // Check if have thumbnail
+        const response = await fetch(imageUrl, { method: 'HEAD' });
+        if (!response.ok) {
+            imageUrl = null;
+            
+            const {prompt_, fid:creator} = getRequestProps(req, [RequestProps.PROMPT, RequestProps.FID]);
+            prompt = prompt_;
+
+            if (prompt && creator)
+            {
                 // Get creator profile from fid
                 profile = (await neynarClient.fetchBulkUsers([creator])).users[0];
                 pfpURL = `https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_jpg,w_168/${encodeURI(profile.pfp_url)}` 
