c036403823745ac5470b3c0f895841d8ffb48443
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-06-14 08:36:52 -0700

    Fix

--- app/api/alea/[eventName]/image/route.tsx
+++ app/api/alea/[eventName]/image/route.tsx
@@ -12,7 +12,15 @@ let fontData = fs.readFileSync(fontPath)
 
 export async function GET(req: NextRequest, { params: { eventName } }: { params: { eventName: string } }) {
     try {
-        const {time:startDate, prompt, fid: creator} = getRequestProps(req, [RequestProps.TIME, RequestProps.PROMPT, RequestProps.FID]);
+        let startDate, prompt, creator;
+        try {
+            const {time, prompt: prompt_, fid} = getRequestProps(req, [RequestProps.TIME, RequestProps.PROMPT, RequestProps.FID]);
+            startDate = time;
+            prompt = prompt_;
+            creator = fid;
+        } catch (error) {
+            console.error(error);
+        }
 
         const now = new Date().getTime() / 1000;
 
@@ -20,37 +28,41 @@ export async function GET(req: NextRequest, { params: { eventName } }: { params:
         const till = startDate - now;
         const hours = Math.ceil(((till) / (60 * 60)) * 10) / 10;   
 
-        let imageUrl = getCldImageUrl({
-            width: 960,
-            height: 600,
-            src: eventName
-        });
-
+        let imageUrl = null;
         let profile;
         let pfpURL;
-        // Check if have thumbnail
-        const response = await fetch(imageUrl, { method: 'HEAD' });
-        if (!response.ok) {
-            imageUrl = null;
-            
-            // Get creator profile from fid
-            profile = (await neynarClient.fetchBulkUsers([creator])).users[0];
-            pfpURL = `https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_jpg,w_168/${encodeURI(profile.pfp_url)}` 
+        
+        if (prompt && creator)
+        {
+            imageUrl = getCldImageUrl({
+                width: 960,
+                height: 600,
+                src: eventName
+            });
+    
+    
+            // Check if have thumbnail
+            const response = await fetch(imageUrl, { method: 'HEAD' });
+            if (!response.ok) {
+                imageUrl = null;
+                
+                // Get creator profile from fid
+                profile = (await neynarClient.fetchBulkUsers([creator])).users[0];
+                pfpURL = `https://res.cloudinary.com/merkle-manufactory/image/fetch/c_fill,f_jpg,w_168/${encodeURI(profile.pfp_url)}` 
+            }
         }
 
         let imageResponse =  new ImageResponse(
             <AleaFrameBase>
                 <div style={{width:'100%', height:'100%', display:'flex', justifyContent:'center', alignItems: 'center'}}>                
-                        {
-                            imageUrl
-                            ? 
-                            <img src={imageUrl} />
-                            :
-                            <div style={{width: 650, height: 310, backgroundColor: 'white', display: 'flex', flexDirection:'column', justifyContent:'center', alignItems: 'center', borderRadius: "75px"}}>
-                                <h1 style={{textAlign:'center', color: 'black', fontSize: 40, fontWeight: 'bold', margin:10}}>{prompt}</h1>
-                                <h2 style={{textAlign:'center', color: 'black', fontSize: 30, fontWeight: 'bold'}}>Created by: @{profile.username} <img style={{ width: 40, maxHeight: 40, marginLeft: 5, marginRight: 5, borderRadius: 50 }} src={pfpURL}/></h2>
-                            </div>
-                        }
+                        {imageUrl
+                        ? 
+                        <img src={imageUrl} />
+                        :
+                        <div style={{width: 650, height: 310, backgroundColor: 'white', display: 'flex', flexDirection:'column', justifyContent:'center', alignItems: 'center', borderRadius: "75px"}}>
+                            <h1 style={{textAlign:'center', color: 'black', fontSize: 40, fontWeight: 'bold', margin:10}}>{prompt}</h1>
+                            <h2 style={{textAlign:'center', color: 'black', fontSize: 30, fontWeight: 'bold'}}>Created by: @{profile.username} <img style={{ width: 40, maxHeight: 40, marginLeft: 5, marginRight: 5, borderRadius: 50 }} src={pfpURL}/></h2>
+                        </div>}
                     <h1 style={{color: 'white', fontSize:20, position:'absolute', bottom:-10, right:15, textAlign:'start', textDecoration:'underline'}}>{now > startDate ? `Event Closed` : `Closes in: ${hours} hrs`}</h1>
                 </div>
             </AleaFrameBase>
