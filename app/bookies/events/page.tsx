afcd250a0b1089ea0582ed8d84af62a12fe792c4
Iman Tanumihardja <34433210+ImanTanumihardja@users.noreply.github.com>
2024-06-16 17:17:16 -0700

    Update page.tsx

--- app/bookies/events/page.tsx
+++ app/bookies/events/page.tsx
@@ -5,17 +5,15 @@ import { Accounts, DatabaseKeys } from "@utils/constants";
 export default async function EventsPage() {
     // Get events from database
     
-    let result = await kv.sscan(`${Accounts.BOOKIES}:${DatabaseKeys.EVENTS}`, 0);
-    let cursor = result[0];
-    let events = result[1];
+    // let result = await kv.sscan(`${Accounts.BOOKIES}:${DatabaseKeys.EVENTS}`, 0);
+    // let cursor = result[0];
+    // let events = result[1];
 
     // while (cursor && cursor !== '0') {
     //     result = await kv.sscan(`${Accounts.BOOKIES}:${DatabaseKeys.EVENTS}`, cursor);
     //     cursor = result[0];
     //     events = events.concat(result[1]);
     // }
-
-    console.log(events);
 
     return(
         <Container className="font-body"> 
